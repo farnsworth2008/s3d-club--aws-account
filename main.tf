65bf35047b7544cbde21d1052631460eb1fe8b15
Mark Farnsworth <farnsworth2008@gmail.com>
2022-10-24 17:56:49 +0000

    0.1.10-s3d-1002 Changed
    - Added `terraform.tf`

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ module "site" {
 # tfsec:ignore:aws-ec2-no-public-egress-sgr
 # tfsec:ignore:aws-ec2-no-public-ingress-sgr
 module "site_group" {
-  source = "github.com/s3d-club/terraform-aws-site-group?ref=0.1.28-1000"
+  source = "github.com/s3d-club/terraform-aws-site-group?ref=0.1.28-s3d-1001"
 
   cidr6s        = ["::/0"]
   cidrs         = ["0.0.0.0/0"]
